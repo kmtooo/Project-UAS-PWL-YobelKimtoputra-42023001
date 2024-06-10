8867529d699a8098e00ec3c8b12a0577a78ad015
kmtooo <yobel.422023001@civitas.ukrida.ac.id>
2024-06-11 00:12:36 +0700

    ini untuk progress ke-3

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TumblerController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('pages.home');
+})->name('home');   
+
+Route::get('/tumbler', function () {
+    return view('pages.plp');
+})->name('plp');  
+
+Route::get('/tumbler/{i}', function () {
+    return view('pages.pdp');
+})->name('pdp');  
